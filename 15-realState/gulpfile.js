ad1735fb5ffe2d0bdd6be7d33301a3bc71d07cdf
Glenn Galicia <glenn.f.g.a@gmail.com>
2026-08-31 19:35:06 -0600

    style: apply prettier formatting

--- 15-realState/gulpfile.js
+++ 15-realState/gulpfile.js
@@ -12,39 +12,38 @@ const { src, dest, watch, series } = gulp;
 
 // Dev — sin cssnano, con sourcemaps
 function css(done) {
-    src('src/scss/app.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({ api: 'modern' }))
-        .pipe(postcss([autoprefixer()]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/css'))
+  src('src/scss/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ api: 'modern' }))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/css'));
 
-    done();
+  done();
 }
 
 // Build — con cssnano, sin sourcemaps
 function build(done) {
-    src('src/scss/app.scss')
-        .pipe(sass({ api: 'modern' }))
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(dest('build/css'))
-    done();
+  src('src/scss/app.scss')
+    .pipe(sass({ api: 'modern' }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('build/css'));
+  done();
 }
 
 function imagenes() {
-    return src('src/img/**/*', { encoding: false })
-        .pipe(dest('build/img', { encoding: false }))
+  return src('src/img/**/*', { encoding: false }).pipe(dest('build/img', { encoding: false }));
 }
 
 function versionWebp() {
-    return src('src/img/**/*.{png,jpg}', { encoding: false })
-        .pipe(webp({ quality: 50 }))
-        .pipe(dest('build/img', { encoding: false }))
+  return src('src/img/**/*.{png,jpg}', { encoding: false })
+    .pipe(webp({ quality: 50 }))
+    .pipe(dest('build/img', { encoding: false }));
 }
 
 function dev() {
-    watch('src/scss/**/*.scss', css);
-    watch('src/img/**/*', imagenes);
+  watch('src/scss/**/*.scss', css);
+  watch('src/img/**/*', imagenes);
 }
 
 export { css, build, dev, imagenes, versionWebp };
